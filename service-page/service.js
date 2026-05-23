3f4093989089afa355bea2b6bf65b275bdd38a39
vanithadurairaj <vanithadurairaj16@gmail.com>
2026-05-23 22:26:12 +0530

    added service page

---
+++ service-page/service.js
@@ -0,0 +1,20 @@
+let titles = document.querySelectorAll(".footer__title");
+
+titles.forEach((title) =>{
+
+   title.addEventListener("click", ()=>{
+
+      if(window.innerWidth <= 800){
+
+         let links =
+           title.nextElementSibling;
+          console.log(links)
+        links.classList.toggle("active");
+
+      }
+
+   });
+
+});
+
+
